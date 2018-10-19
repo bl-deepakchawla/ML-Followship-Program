ca20ab73cea4914294ac1cd78f8bbdeca9a9b76e
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-19 12:08:39 +0530

    Create function revere_order()

---
+++ Basic Python/1. WAP which accepts the user's first and last name and print them in reverse order with a space between them..py
@@ -0,0 +1,6 @@
+def reverse_order():
+    fname = input("Enter your first name: ")
+    lname = input("Enter your last name: ")
+    print("".join(reversed(fname)), "".join(reversed(lname)))
+
+reverse_order()
