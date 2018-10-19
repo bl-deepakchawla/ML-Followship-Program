fbb9ff4c524715d9d089718ee40046ce3b50c83f
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-19 12:33:54 +0530

    Create function display_colors()

---
+++ Basic Python/3. WAP  to display the first and last colors from the following list..py
@@ -0,0 +1,8 @@
+def display_colors(color_value):
+
+    print("First Color: ", color_value[0])
+    print("Last Color: ", color_value[-1])
+
+
+colors_list = ["Red", "Green", "White", "Black"]
+display_colors(colors_list)
