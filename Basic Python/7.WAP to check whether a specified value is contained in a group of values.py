983678822fbdcb95b462d134a7970c54958036de
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-19 16:03:23 +0530

    Create function check_contain_value()

---
+++ Basic Python/7.WAP to check whether a specified value is contained in a group of values.py
@@ -0,0 +1,16 @@
+def check_contain_value(first_list, second_list):
+    if 3 in first_list:
+        print('True')
+    else:
+        print('False')
+
+    if -1 in second_list:
+        print('True')
+    else:
+        print('False')
+
+
+first_list = [1, 5, 8, 3]
+second_list = [1, 5, 8, 3]
+check_contain_value(first_list, second_list)
+
