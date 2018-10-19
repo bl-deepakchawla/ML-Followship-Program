c3eb81d4349f4fce3063b6278738341421379b20
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-19 15:59:55 +0530

    Remove input user input functionality.

--- Basic Python/6 . WAP to calculate number of days between two dates.py
+++ Basic Python/6 . WAP to calculate number of days between two dates.py
@@ -1,15 +1,13 @@
 from datetime import date
 
 
-def days_btw_two_dates():
-    temp_start_date = input("Enter starting date(year/month/day): ")
-    temp_end_date = input("Enter ending date(year/month/day): ")
-    start_date = temp_start_date.split('/')
-    end_date = temp_end_date.split('/')
+def days_btw_two_dates(start_date, end_date):
     total_days = (date(int(start_date[0]),int(start_date[1]),int(start_date[-1])) - date(int(end_date[0]),int(end_date[1]),int(end_date[-1]))).days
-    print("Total no. of days between ", temp_start_date, " and ", temp_end_date, " is ", total_days)
+    print("Total no. of days between ", start_date, " and ", end_date, " is ", total_days)
 
 
-days_btw_two_dates()
+start_date = (2014, 7, 11)
+end_date = (2014, 7, 2)
+days_btw_two_dates(start_date, end_date)
 
 
