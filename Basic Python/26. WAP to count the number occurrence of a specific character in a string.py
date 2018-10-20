1c61c41df3036682ce87ff39dda56ffecc94ef13
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-20 12:19:54 +0530

    Create function count_char_in_string()

---
+++ Basic Python/26. WAP to count the number occurrence of a specific character in a string.py
@@ -0,0 +1,8 @@
+def count_char_in_string(str, char):
+    return str.count(char)
+
+
+str = "Google"
+char = "o"
+result = count_char_in_string(str, char)
+print("No of", char, "in word", str, "is", result)
