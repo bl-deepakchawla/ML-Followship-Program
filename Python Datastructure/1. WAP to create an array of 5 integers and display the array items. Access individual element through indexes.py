33771ef9d17aba58ab06de06845cb2d2a207cfb3
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-22 11:25:43 +0530

    Create function display_access_array()

---
+++ Python Datastructure/1. WAP to create an array of 5 integers and display the array items. Access individual element through indexes.py
@@ -0,0 +1,10 @@
+def display_access_array(arr_value):
+    print("First Index value:", arr_value[0])
+    print("Second Index value:", arr_value[1])
+    print("Third Index value:", arr_value[2])
+    print("Fourth Index value:", arr_value[3])
+    print("Fifth Index value:", arr_value[4])
+
+
+arr = [11, 12, 13, 14, 15]
+display_access_array(arr)
