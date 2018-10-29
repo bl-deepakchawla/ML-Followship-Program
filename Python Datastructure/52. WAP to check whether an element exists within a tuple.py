be4a5eb2b7fc85ddd84de011084168bda87b4029
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 17:23:37 +0530

    Create function check_value_exists_tuple()()

---
+++ Python Datastructure/52. WAP to check whether an element exists within a tuple.py
@@ -0,0 +1,9 @@
+def check_value_exists_tuple():
+    tuple_value = (12, 13, 14, 15, 16)
+    if 12 in tuple_value:
+        print(True)
+    else:
+        print(False)
+
+
+check_value_exists_tuple()
