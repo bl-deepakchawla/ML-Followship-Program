ea7c6a481a43b56f20832f5be019d6743c5a4a55
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 17:35:02 +0530

    Create function slice_tuple()

---
+++ Python Datastructure/55. WAP to slice a tuple.py
@@ -0,0 +1,8 @@
+def slice_tuple():
+    tuple_value = (12, 13, 14, 15, 16)
+    print("Tuple values before slicing item from it", tuple_value)
+    slice_tuple_value = tuple_value[1:3]
+    print("Tuple values after slicing item from it", slice_tuple_value)
+
+
+slice_tuple()
