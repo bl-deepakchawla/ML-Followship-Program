ee86911df0eff455c8e26f7f1f6cac877f8d88c6
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 17:12:01 +0530

    Create function unpack_tuple_serveral_variable()

---
+++ Python Datastructure/49. WAP to unpack a tuple in several variables.py
@@ -0,0 +1,9 @@
+def unpack_tuple_serveral_variable():
+    tuplex = 4, 8, 3
+    print(tuplex)
+    n1, n2, n3 = tuplex
+    print(n1 + n2 + n3)
+    n1, n2, n3, n4 = tuplex
+
+
+unpack_tuple_serveral_variable()
