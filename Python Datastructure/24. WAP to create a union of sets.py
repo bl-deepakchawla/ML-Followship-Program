6011950fc969d2085f480c8f1b96d92dcd87de31
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 11:43:14 +0530

    Create function union_of_sets()

---
+++ Python Datastructure/24. WAP to create a union of sets.py
@@ -0,0 +1,7 @@
+def union_of_sets():
+    set_value1 = set(["User1", "User2", "User3", "User4"])
+    set_value2 = set(["User1", "User3"])
+    print("Union of set_value1 with set_value2 ", set_value1.union(set_value2))
+
+
+union_of_sets()
