3533b6613515a2d5cbbd7c732a9725bd3ec20bb8
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 11:44:26 +0530

    Create function difference_of_sets()

---
+++ Python Datastructure/25. WAP to create set difference.py
@@ -0,0 +1,7 @@
+def difference_of_sets():
+    set_value1 = set(["User1", "User2", "User3", "User4"])
+    set_value2 = set(["User1", "User3"])
+    print("Difference of set_value1 with set_value2 ", set_value1.difference(set_value2))
+
+
+difference_of_sets()
