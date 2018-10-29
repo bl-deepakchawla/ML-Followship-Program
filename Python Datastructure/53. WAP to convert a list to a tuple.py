2c6d5e932b24abd75012551092b7ec31f0e7aa1f
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 17:27:08 +0530

    Create function tuple_to_list()

---
+++ Python Datastructure/53. WAP to convert a list to a tuple.py
@@ -0,0 +1,8 @@
+def tuple_to_list():
+    tuple_value = (12, 13, 14, 15, 16)
+    print("Tuple values and ", tuple_value, "its data type ",type(tuple_value))
+    list_value = list(tuple_value)
+    print("Tuple values and ", list_value, "its data type ",type(list_value))
+
+
+tuple_to_list()
