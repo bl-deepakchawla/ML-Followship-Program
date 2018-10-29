7bf027f3667ecc9cedd61f3e33bce78174b281fa
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 15:01:43 +0530

    Create function clone_or_copy_list()

---
+++ Python Datastructure/36. WAP to clone or copy a list.py
@@ -0,0 +1,9 @@
+def clone_or_copy_list():
+    list_value = [12, 13, 14, 15, 16]
+    cloned_list_value = list_value.copy()
+    print("List values ", list_value)
+    print("Cloned list values", cloned_list_value)
+
+
+clone_or_copy_list()
+
