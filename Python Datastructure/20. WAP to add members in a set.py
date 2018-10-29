0ff4992b08239022ff2eb2f87f5682a4e960ef71
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 11:21:45 +0530

    Create function add_member_sets()

---
+++ Python Datastructure/20. WAP to add members in a set.py
@@ -0,0 +1,8 @@
+def add_member_sets():
+    set_value = set(["User1", "User2", "User3"])
+    new_value = "User4"
+    print("Set value before adding User4 are ", set_value)
+    set_value.add(new_value)
+    print("Set value after adding User4 are ", set_value)
+
+add_member_sets()
