b73352d498dcfcdcb98bdd5e43d8f4d98fa85926
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 12:02:36 +0530

    Create function max_min_value_in_set()

---
+++ Python Datastructure/29. WAP to find maximum and the minimum value in a set.py
@@ -0,0 +1,7 @@
+def max_min_value_in_set():
+    set_value = set([1, 2, 3, 4 , 5, 6])
+    print("Maximum value in the set", max(set_value))
+    print("Minimum value in the set", min(set_value))
+
+
+max_min_value_in_set()
