7e0bfd9b6771462ebc3efa8658424f606df6813a
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 12:17:08 +0530

    Create function list_multiply()

---
+++ Python Datastructure/31. WAP to multiplies all the items in a list.py
@@ -0,0 +1,9 @@
+def list_multiply():
+    list_value = [1, 2, 3, 4]
+    result_list = 1
+    for value in list_value:
+        result_list *= value
+    print("Multiply list elements with list values ", result_list)
+
+
+list_multiply()
