55457783da6a391540c0a59e59383336eaa8ccce
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 11:49:38 +0530

    Create function clear_sets()

---
+++ Python Datastructure/27. WAP to clear a set.py
@@ -0,0 +1,7 @@
+def clear_sets():
+    set_value = set(["User1", "User2", "User3", "User4"])
+    print("Set value before cleat whole set ", set_value)
+    print("Set value after clear whole set ", set_value.clear())
+
+
+clear_sets()
