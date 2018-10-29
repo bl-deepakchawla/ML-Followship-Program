0f413bffbaa5084cb60a7ea7cb14bfef85774fb0
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 17:08:14 +0530

    Create function create_tuple_with_different_values()

---
+++ Python Datastructure/48. WAP to create a tuple with different data types.py
@@ -0,0 +1,6 @@
+def create_tuple_with_different_values():
+    tuple_list = (12, "1", 14.00, "abc", [10,11], {'1': 'abc'})
+    print("Tuple values", tuple_list)
+
+
+create_tuple_with_different_values()
