a82f5777029645e687411a0bcc1a0106545948bd
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-29 11:36:01 +0530

    Create function intersection_of_sets()

---
+++ Python Datastructure/23. WAP to create an intersection of sets.py
@@ -0,0 +1,7 @@
+def intersection_of_sets():
+    set_value1 = set(["User1", "User2", "User3", "User4"])
+    set_value2 = set(["User1", "User3"])
+    print("Set value before remove User4 are ", set_value1.intersection(set_value2))
+
+
+intersection_of_sets()
