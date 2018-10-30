93f7468d4af0621c10d507ba4baed8db16f8dce1
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-30 16:15:58 +0530

    Create function matrix_transpose()

---
+++ Linear Algebra/6. WAP find transpose matrix of matrix Y in problem 1.py
@@ -0,0 +1,11 @@
+def matrix_transpose(l_mat1):
+    return [[row[i] for row in l_mat1] for i in range(len(l_mat1[0]))]
+
+
+g_mat1 = [[5, 8, 1],
+          [6, 7, 3],
+          [4, 5, 9]]
+
+g_resultant_mat = matrix_transpose(g_mat1)
+print("First Matrix is", g_mat1)
+print("Transpose of first matrix is", g_resultant_mat)
