e92fe27a4ffc26177f55af77d95335fb465b86a6
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-30 12:58:42 +0530

    Create function reverse_string()

---
+++ Python Datastructure/67. WAP to reverse a string.py
@@ -0,0 +1,9 @@
+def reverse_string(l_str_value):
+    l_reverse_str = l_str_value[::-1]
+    return l_reverse_str
+
+
+g_str_value = "AI is new electricity. AI can do all things"
+g_reversed_str = reverse_string(g_str_value)
+print("String is ", g_str_value)
+print("Reversed string is ", g_reversed_str)
