a94936b12df3de2bd1c9c1b676db6869ee07cb0e
Deepak Chawla <deepakchawla35@gmail.com>
2018-10-30 15:54:59 +0530

    Create function multiply_two_matrix()

---
+++ Linear Algebra/4. WAP to multiply matrices in problem 1.py
@@ -0,0 +1,21 @@
+def multiply_two_matrix(l_mat1, l_mat2):
+    l_resultant_mat = [[0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0]]
+    for value1 in range(0, len(l_mat1)):
+        for value2 in range(0, len(l_mat1[0])):
+            l_resultant_mat[value1][value2] = l_mat1[value1][value2] * l_mat2[value1][value2]
+    return l_resultant_mat
+
+
+g_mat1 = [[12, 7, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+g_mat2 = [[5, 8, 1],
+          [6, 7, 3],
+          [4, 5, 9]]
+
+g_resultant_mat = multiply_two_matrix(g_mat1, g_mat2)
+print("First Matrix is", g_mat1)
+print("Second Matrix is", g_mat2)
+print("Sum of both matrix is", g_resultant_mat)
